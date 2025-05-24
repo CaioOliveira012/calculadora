993f4539284371ba40c8fbe36d014b15005f117b
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-05-23 23:46:04 -0300

    fórmulas e histórico dos resultados

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,7 @@
 def calculadora():
+
+    historico = []
+
     while True:
         n1 = float(input("Digite o primeiro número: "))
         n2 = float(input("Digite o segundo número: "))
@@ -8,22 +11,54 @@ def calculadora():
         print("2 - Subtração")
         print("3 - Multiplicação")
         print("4 - Divisão")
+        print("5 - Resto da divisão")
+        print("6 - Consultar histórico")
+        print("7 - Raiz quadrada")
+        print("8 - Potência(n1^n2)")
+        print("9 - Usar último resultado como n1")
+        print("10 - Salvar histórico em arquivo")
         print("0 - Sair")
 
         escolha = input("Digite sua escolha: ")
 
         match escolha:
             case'1':
-                print("Resultado: ")
+                c = n1+n2
+                print("Resultado: ", c)
+                historico.append(f"{n1} + {n2} = {c}")
             case '2':
-                print("Resultado: ")
+                c = n1-n2
+                print("Resultado: ", c)
+                historico.append(f"{n1} - {n2} = {c}")
             case'3':
-                print("Resultado: ")
+                c = n1*n2
+                print("Resultado: ", c)
+                historico.append(f"{n1} * {n2} = {c}")
             case'4':
-                print("Resultado: ")
+                if n2 != 0:
+                    c = n1/n2
+                    print("Resultado: ", c)
+                    historico.append(f"{n1} / {n2} = {c}")
+                else:
+                    print("Divisão por 0")
+            case'5':
+                if n2 != 0:
+                    c = n1%n2
+                    print("Resultado: ", c)
+                    historico.append(f"{n1} % {n2} = {c}")
+                else:
+                    print("Divisão por 0")
+            case'6':
+                
+                print("Histórico: ", historico)
             case'0':
                 print("Saindo da calculadora")
                 break
             case _:
                 print("Opção inválida. Tente Novamente")
+                
+        historico.append(c)
+        if len(historico) > 5:
+            historico.pop(0)
+
 calculadora()
